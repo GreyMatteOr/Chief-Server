4354aa5758404f2f7b447037b7dde60affe167db
Matthew Lane <themplane@gmail.com>
2021-01-21 20:45:06 -0800

    Add Base File Infrastructure

---
+++ Players/Blue.js
@@ -0,0 +1,7 @@
+import Player from './Player'
+
+export default class Blue extends Player {
+  constructor() {
+
+  }
+}
---
+++ Players/index.js
@@ -0,0 +1,6 @@
+import Blue from './Blue.js'
+import Green from './Green.js'
+import Red from './Red.js'
+import Yellow from './Yellow.js'
+
+export {Blue, Green, Red, Yellow};
