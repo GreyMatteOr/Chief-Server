29901356961b4df2bd5af5b7b98a1ce485c83755
Matthew Lane <themplane@gmail.com>
2021-01-23 16:51:54 -0800

    Add DB integration and remove `module` typing

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pgp = require('pg-promise')(/* options */)
+app.use(express.json());
+app.use(cors());
+app.set('port', process.env.PORT || 3000);
 const server = require('http').createServer(app);
+
+let rules = {}
 let users = {};
 
 const io = require("socket.io")(server, {
@@ -11,9 +17,15 @@ const io = require("socket.io")(server, {
   }
 });
 
-app.use(express.json());
-app.use(cors());
-app.set('port', process.env.PORT || 3000);
+const db = pgp('postgres://matthewlane:1234@localhost:5432/chief')
+
+db.one('SELECT $1 AS value', 123)
+  .then(function (data) {
+    console.log('DATA:', data.value)
+  })
+  .catch(function (error) {
+    console.log('ERROR:', error)
+  })
 
 io.on( 'connect', ( socket ) => {
   console.log('A user has connected!');
